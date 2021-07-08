aa70a718b7469b0549745292ec96ca69f9f18c83
Kevin Perex <mr.kv17.kp@gmail.com>
2021-07-08 14:09:57 -0400

    Documentos

--- src/app/documents/documents.page.ts
+++ src/app/documents/documents.page.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentsPage implements OnInit {
 
+  uploadedFile: boolean = false;
+  imgPreview: string | ArrayBuffer;
+  imgName: string;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+updateFile(event){
+    if (event.target.files.length > 0){
+      const file = event.target.files[0];
+      this.imgName = event.target.files[0].name;
+      // preview de la img
+      const reader = new FileReader();
+      // leo el archivo seleccionado
+      reader.onload = e => this.imgPreview = reader.result;
+      reader.readAsDataURL(file);
+      this.uploadedFile = true;
+    }
+  }
 }

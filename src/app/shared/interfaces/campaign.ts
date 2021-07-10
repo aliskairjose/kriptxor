376cf52ed94117ba522a418894e020374506fcd8
Sabas Vega <sabascarloseduardo@gmail.com>
2021-07-10 14:16:51 -0500

    solved merge

--- src/app/shared/interfaces/campaign.ts
+++ src/app/shared/interfaces/campaign.ts
@@ -1,42 +1,12 @@
-import {DateAt, Status} from './response';
+import {DateAt} from './response';
 import {MasterClient} from './client';
-import {Seller} from './seller';
+//import {Seller} from './seller';
 
-export class Campaign {
-  id: number;
-  name: string;
-  type: number;
-  condition: number;
-  start_date: string;
-  messages: number;
-  text: string;
-  id_user: number;
-  success: string;
-  created_at: DateAt;
-  updated_at: DateAt;
-  campaign_clients_count: number;
-  campaign_clients_auth_count: number;
-  status_name: string;
-  quotes: number;
-  user_create: string;
-  user_update: string;
-  deleted_at: string;
-  campaigns_clients: CampaignClient[]
-}
 export class CampaignClient{
   masterClient: MasterClient;
   status: Status;
   last: Last;
   seller: Seller;
-}
-export class Last{
-  id: number;
-  id_campaign_client: number;
-  id_user: number;
-  observation: string;
-  condition: string;
-  created_at: string;
-  url_file: string;
 }
 export interface Campaign {
   id?: number;
@@ -47,8 +17,8 @@ export interface Campaign {
   cantidad_mensajes?: number;
   texto?: string;
   id_user?: number;
-  created_at?: string;
-  updated_at?: string;
+  created_at: string;
+  updated_at: string;
   user_create?: string;
   user_update?: string;
   success?: string;
@@ -133,3 +103,4 @@ export interface Detail {
   created_at?: string;
   url_file?: string | null;
   vendedor?: Seller;
+}

8941728d8ecf3810cb4c08920eb7bb41aa5918d5
Kevin Perex <mr.kv17.kp@gmail.com>
2021-07-09 20:11:57 -0400

    Maqueta de notas terminada, y con implementacion de scroll infinto solo en el contenido de la card. quedando solo la integracion de la maqueta

--- src/app/notes/notes.page.ts
+++ src/app/notes/notes.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-notes',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notes.page.scss'],
 })
 export class NotesPage implements OnInit {
+   @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+    //if (data.length == 1000) {
+      //  event.target.disabled = true;
+      //}
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
   }
 
 }

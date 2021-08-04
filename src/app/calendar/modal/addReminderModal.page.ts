286c743b27cf6d2d73fe013475742564918d5361
Jonathan Alvarado <jonathanalvarado1407@gmail.com>
2021-08-03 23:33:18 -0400

    feat: add edit and delete funcionality to calendar
    [] Pending show reminder data in inputs when editing

--- src/app/calendar/modal/addReminderModal.page.ts
+++ src/app/calendar/modal/addReminderModal.page.ts
@@ -1,58 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { USER } from 'src/app/shared/constants/constants';
-import { CalendarService } from 'src/app/shared/services/calendar.service';
+import {
+  CalendarService,
+  ReminderParams,
+} from 'src/app/shared/services/calendar.service';
 import { StorageService } from 'src/app/shared/services/storage.service';
 import { CommonService } from '../../shared/services/common.service';
 
-@Component( {
+@Component({
   selector: 'app-reminder-modal',
   templateUrl: './addReminderModal.page.html',
-  styleUrls: [ './styles.scss' ],
-} )
+  styleUrls: ['./styles.scss'],
+})
 export class AddReminderModalComponent implements OnInit {
   public title = '';
   public selectedHour = '';
   public duration = null;
   public date: string;
+  public isEditing = false;
+  public reminderId: number;
 
   constructor(
     private modal: ModalController,
     private calendarService: CalendarService,
     private storage: StorageService,
     private common: CommonService
-  ) { }
+  ) {}
 
-  ngOnInit() { }
+  ngOnInit() {}
 
-  public setHour( date: string ) {
-    console.log( date );
-    const newDate = new Date( date );
-    this.selectedHour = `${this.zeroBased( newDate.getHours() )}:${this.zeroBased(
+  public setHour(date: string) {
+    console.log(date);
+    const newDate = new Date(date);
+    this.selectedHour = `${this.zeroBased(newDate.getHours())}:${this.zeroBased(
       newDate.getMinutes()
     )}:00`;
   }
 
   public async saveReminder(): Promise<void> {
-    if ( !( await this.validateDate() ) ) return;
+    if (!(await this.validateDate())) return;
 
-    const userId = ( ( await this.storage.get( USER ) ) as any ).id;
+    let data = {
+      title: this.title,
+      date: `${this.date} ${this.selectedHour}`,
+      duration: parseInt(this.duration, 10),
+    } as ReminderParams;
 
-    const data = {
-      attributes: {
-        title: this.title,
-        date: `${this.date} ${this.selectedHour}`,
-        duration: parseInt(this.duration),
-        userId,
-      },
-    };
+    if (!this.isEditing) {
+      const userId = ((await this.storage.get(USER)) as any).id;
 
-    this.calendarService.setReminder( data ).subscribe( async ( response ) => {
-      console.log( response );
-      await this.common.presentToast( { message: response.message } );
+      data = { ...data, userId };
 
-      this.close();
-    } );
+      this.calendarService.setReminder(data).subscribe(async (response) => {
+        console.log(response);
+        await this.common.presentToast({ message: response.message });
+
+        this.close();
+      });
+    } else {
+      this.calendarService
+        .updateReminder(this.reminderId, data)
+        .subscribe(async (response) => {
+          console.log(response);
+          await this.common.presentToast({ message: response.message });
+
+          this.close();
+        });
+    }
   }
 
   async close() {
@@ -61,24 +76,24 @@ export class AddReminderModalComponent implements OnInit {
 
   private async validateDate(): Promise<boolean> {
     let message = '';
-    if ( this.title === '' ) {
+    if (this.title === '') {
       message = 'El título del recordatorio no debe estar vacío';
-      this.common.presentToast( { message } );
+      this.common.presentToast({ message });
       return false;
-    } else if ( this.selectedHour === '' ) {
+    } else if (this.selectedHour === '') {
       message = 'La hora de inicio del recordatorio no debe estar vacía';
-      this.common.presentToast( { message } );
+      this.common.presentToast({ message });
       return false;
-    } else if ( this.duration === null ) {
+    } else if (this.duration === null) {
       message = 'La duración del recordatorio no debe estar vacío';
-      this.common.presentToast( { message } );
+      this.common.presentToast({ message });
       return false;
     }
 
     return true;
   }
 
-  private zeroBased( value: string | number ): string {
-    return `0${value}`.slice( -2 );
+  private zeroBased(value: string | number): string {
+    return `0${value}`.slice(-2);
   }
 }
